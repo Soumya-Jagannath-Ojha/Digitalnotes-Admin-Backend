4d5d0ab300cd1e273c16b0b95ee91b13b93a37a7
Soumya <soumyajagannathojha7@gmail.com>
2024-12-01 12:35:55 +0530

    cors policy

--- src/server.js
+++ src/server.js
@@ -21,6 +21,10 @@ app.use(
   })
 );
 
+app.options('*', cors({
+  origin: "https://diginotesadmin.netlify.app",
+  credentials: true,
+}));
 
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true, limit: "5mb" }));
